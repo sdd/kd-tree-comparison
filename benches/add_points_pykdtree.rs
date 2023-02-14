82a7d5fb0ae2100bfed9a9564ca93ab2e813ba81
Scott Donnelly <scott@donnel.ly>
2023-02-14 22:54:23 +0000

    feat: add benchmark for pykdtree add_points

---
+++ benches/add_points_pykdtree.rs
@@ -0,0 +1,72 @@
+use criterion::measurement::WallTime;
+use criterion::{
+    black_box, criterion_group, criterion_main, AxisScale, BatchSize, BenchmarkGroup, BenchmarkId,
+    Criterion, PlotConfiguration, Throughput,
+};
+use criterion_polyglot::{BenchSpec, CriterionPolyglotExt};
+
+use kiddo_v2::batch_benches;
+use rand::distributions::{Distribution, Standard};
+
+const BUCKET_SIZE: usize = 32;
+
+macro_rules! bench_empty_float {
+    ($group:ident, $a:ty, $t:ty, $k:tt, $idx: ty, $size:tt, $subtype: expr) => {
+        bench_add_to_empty_float::<$k>(&mut $group, $size, &format!("pykdtree {}", $subtype));
+    };
+}
+
+pub fn add_to_empty(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Add to Empty Tree");
+
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+
+    group.plot_config(plot_config);
+
+    batch_benches!(
+        group,
+        bench_empty_float,
+        [(f64, 2), (f64, 3), (f64, 4)],
+        [
+            (100, u16, u16),
+            (1_000, u16, u16),
+            (10_000, u16, u16),
+            (100_000, u32, u16),
+            (1_000_000, u32, u32)
+        ]
+    );
+
+    /*batch_benches!(
+        group,
+        bench_empty_float,
+        [(f64, 3)],
+        [
+            (100_000, u32, u16)
+        ]
+    );*/
+
+    group.finish();
+}
+
+fn bench_add_to_empty_float<const K: usize>(
+    group: &mut BenchmarkGroup<WallTime>,
+    qty_to_add: usize,
+    subtype: &str,
+) where
+    Standard: Distribution<[f64; K]>,
+{
+    group.python_benchmark(
+        &*format!("{}/{}", &subtype, &qty_to_add),
+        BenchSpec::new(r#"
+kd_tree = KDTree(data_pts)
+        "#)
+        .with_global_init(&*format!(r#"
+from pykdtree.kdtree import KDTree
+import numpy as np
+data_pts = np.random.rand({}, {})
+        "#, &qty_to_add, K))
+    );
+}
+
+criterion_group!(benches, add_to_empty);
+criterion_main!(benches);
